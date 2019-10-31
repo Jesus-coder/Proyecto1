d5c7aa503174744bec9991cdfc9d183e1a52724d
sergiorueda99 <55807705+sergiorueda99@users.noreply.github.com>
2019-10-31 18:23:13 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -4,13 +4,17 @@
 	<title></title>
 </head>
 <body>
-	<div style="position: absolute; right: 110px; top: 8px">
+	<div style="position: absolute; right: 180px; top: 12px; color: white;">
 		<?php
 		//Cerrar sesion de ".$_SESSION['nombre']."
 		//Utilizar la variable $_SESSION anteriormente configurada
 		session_start();
 		if(isset($_SESSION['nombre'])){
-			echo "Cerrar sesion de ".$_SESSION['nombre']."<a href='logout.php'><img src='imagenes/salir.jpg'></a>&nbsp;&nbsp;";
+			echo "Cerrar sesion de ".$_SESSION['nombre']."
+			"?>
+			</div>
+			<div style="position: absolute; right: 110px; top: 8px">
+			<?php echo "<a href='logout.php'><img src='imagenes/salir.jpg'></a>&nbsp;&nbsp;";
 		}else{
 			header("Location: index.php");
 		}
