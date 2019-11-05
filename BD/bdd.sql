f18e73e8ce5e82adb33cfbc45393a5023979ad2e
Jesus <10000657.joan23@fje.edu>
2019-11-05 16:22:44 +0100

    bdd fix

--- BD/bdd.sql
+++ BD/bdd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-10-2019 a las 16:46:54
--- Versión del servidor: 10.1.31-MariaDB
--- Versión de PHP: 7.2.3
+-- Tiempo de generación: 05-11-2019 a las 16:20:08
+-- Versión del servidor: 10.1.28-MariaDB
+-- Versión de PHP: 5.6.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,37 +25,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tbl_usuarios`
---
-
-CREATE TABLE `tbl_usuarios` (
-  `id_usuarios` int(4) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `nombre_usuarios` varchar(25) NOT NULL,
-  `pwd_usuarios` varchar(50) NOT NULL,
-  `estado_usuarios` varchar(25) NOT NULL,
-  `admin_usuarios` int (1) NOT NULL,
-  `informatico_usuarios` int (1) NOT NULL,
-  `normal_usuarios` int (1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `tbl_usuarios`
---
-
-INSERT INTO `tbl_usuarios` VALUES
-(1,'Gerard','81dc9bdb52d04dc20036dbd8313ed055','enabled',1,0,0),
-(2,'Sergio','81dc9bdb52d04dc20036dbd8313ed055','enabled',1,0,0),
-(3,'Victor','81dc9bdb52d04dc20036dbd8313ed055','enabled',0,1,0),
-(4,'Jesus','81dc9bdb52d04dc20036dbd8313ed055','enabled',0,1,0),
-(5,'Juanma','81dc9bdb52d04dc20036dbd8313ed055','enabled',0,0,1),
-(6,'Daniel','81dc9bdb52d04dc20036dbd8313ed055','enabled',0,0,1);
-
---
--- Estructura de tabla para la tabla `tbl_incidencias`
+-- Estructura de tabla para la tabla `tbl_indicencias`
 --
 
 CREATE TABLE `tbl_indicencias` (
-  `id_incidencias` int(4) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `id_incidencias` int(4) NOT NULL,
   `inicio_incidencias` date NOT NULL,
   `final_incidencias` date NOT NULL,
   `informe_incidencias` varchar(300) NOT NULL,
@@ -63,59 +37,84 @@ CREATE TABLE `tbl_indicencias` (
   `id_usuarios` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Estructura de tabla para la tabla `tbl_reservas`
---
-
-CREATE TABLE `tbl_reservas` (
-  `id_reservas` int(4) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `inicio_reservas` DATE NOT NULL,
-  `final_reservas` DATE NOT NULL,
-  `id_usuarios` int(4) NOT NULL,
-  `id_recursos` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `tbl_recursos`
 --
 
 CREATE TABLE `tbl_recursos` (
-  `id_recursos` int(4) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `nombre_recursos` varchar(25) NOT NULL,
-  `estado_recursos` varchar(25) NOT NULL,
-  `id_tiporecursos` int(4) NOT NULL
+  `id_recursos` int(4) NOT NULL,
+  `nombre_recursos` varchar(25) NOT NULL COMMENT 'Nombre del recurso',
+  `estado_recursos` enum('disponible','ocupado','incidencia') NOT NULL COMMENT 'Estado',
+  `id_tiporecursos` int(4) NOT NULL COMMENT 'Tipo'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `tbl_recursos`
 --
 
-INSERT INTO `tbl_recursos` VALUES
-(1,'Yoga','disponible',1),
-(2,'Baile','disponible',1),
-(3,'Presentaciones','disponible',1),
-(4,'Gimnasio','disponible',1),
-(5,'Informatica_1','disponible',1),
-(6,'Informatica_2','disponible',1),
-(7,'Cocina','disponible',1),
-(8,'Despacho_1','disponible',1),
-(9,'Despacho_2','disponible',1),
-(10,'Actos','disponible',1),
-(11,'Reuniones','disponible',1),
-(12,'Proyector_portatil_1','disponible',2),
-(13,'Proyector_portatil_2','disponible',2),
-(14,'Portatil_1','disponible',2),
-(15,'Portatil_2','disponible',2),
-(16,'Portatil_3','disponible',2),
-(17,'Mobil_1','disponible',2),
-(18,'Mobil_2','disponible',2);
+INSERT INTO `tbl_recursos` (`id_recursos`, `nombre_recursos`, `estado_recursos`, `id_tiporecursos`) VALUES
+(1, 'Yoga', 'disponible', 1),
+(2, 'Baile', 'disponible', 1),
+(3, 'Presentaciones', 'disponible', 1),
+(4, 'Gimnasio', 'ocupado', 1),
+(5, 'Informatica_1', 'ocupado', 1),
+(6, 'Informatica_2', 'disponible', 1),
+(7, 'Cocina', 'disponible', 1),
+(8, 'Despacho_1', 'ocupado', 1),
+(9, 'Despacho_2', 'ocupado', 1),
+(10, 'Actos', 'disponible', 1),
+(11, 'Reuniones', 'disponible', 1),
+(12, 'Proyector_portatil_1', 'ocupado', 2),
+(13, 'Proyector_portatil_2', 'ocupado', 2),
+(14, 'Portatil_1', 'disponible', 2),
+(15, 'Portatil_2', 'ocupado', 2),
+(16, 'Portatil_3', 'ocupado', 2),
+(17, 'Mobil_1', 'disponible', 2),
+(18, 'Mobil_2', 'disponible', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_reservas`
+--
+
+CREATE TABLE `tbl_reservas` (
+  `id_reservas` int(4) NOT NULL,
+  `inicio_reservas` datetime DEFAULT NULL,
+  `final_reservas` datetime DEFAULT NULL,
+  `id_usuarios` int(4) NOT NULL,
+  `id_recursos` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbl_reservas`
+--
+
+INSERT INTO `tbl_reservas` (`id_reservas`, `inicio_reservas`, `final_reservas`, `id_usuarios`, `id_recursos`) VALUES
+(1, '2019-10-31 00:00:00', '2019-10-31 00:00:00', 1, 4),
+(8, '2019-11-05 01:20:00', '2019-11-05 01:20:20', 1, 8),
+(9, '2019-11-05 01:55:50', '2019-11-05 15:52:00', 1, 1),
+(10, '2019-11-05 01:55:50', '2019-11-05 15:52:15', 1, 2),
+(11, '2019-11-05 02:00:55', '2019-11-05 15:52:28', 1, 3),
+(12, '2019-11-05 02:00:55', NULL, 1, 4),
+(13, '2019-11-05 13:10:14', NULL, 2, 12),
+(14, '2019-11-05 13:10:14', NULL, 2, 13),
+(15, '2019-11-05 13:28:00', NULL, 1, 9),
+(16, '2019-11-05 13:30:32', NULL, 1, 15),
+(17, '2019-11-05 13:30:32', NULL, 1, 16),
+(18, '2019-11-05 15:41:53', NULL, 1, 5),
+(19, '2019-11-05 15:42:23', NULL, 1, 8);
+
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `tbl_tiporecursos`
 --
 
 CREATE TABLE `tbl_tiporecursos` (
-  `id_tiporecursos` int(4) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `id_tiporecursos` int(4) NOT NULL,
   `nombre_tiporecursos` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -123,58 +122,136 @@ CREATE TABLE `tbl_tiporecursos` (
 -- Volcado de datos para la tabla `tbl_tiporecursos`
 --
 
-INSERT INTO `tbl_tiporecursos` VALUES
-(1,'Sala'),
-(2,'Objeto');
+INSERT INTO `tbl_tiporecursos` (`id_tiporecursos`, `nombre_tiporecursos`) VALUES
+(1, 'Sala'),
+(2, 'Objeto');
+
+-- --------------------------------------------------------
 
 --
--- Creacion de relaciones
+-- Estructura de tabla para la tabla `tbl_usuarios`
 --
 
-ALTER TABLE tbl_indicencias
-	ADD CONSTRAINT FK_usuarios_incidencias FOREIGN KEY (id_usuarios)
-	REFERENCES tbl_usuarios(id_usuarios);
+CREATE TABLE `tbl_usuarios` (
+  `id_usuarios` int(4) NOT NULL,
+  `nombre_usuarios` varchar(25) NOT NULL,
+  `pwd_usuarios` varchar(50) NOT NULL,
+  `estado_usuarios` varchar(25) NOT NULL,
+  `admin_usuarios` int(1) NOT NULL,
+  `informatico_usuarios` int(1) NOT NULL,
+  `normal_usuarios` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE tbl_indicencias
-	ADD CONSTRAINT FK_recursos_incidencias FOREIGN KEY (id_recursos)
-	REFERENCES tbl_recursos(id_recursos);
+--
+-- Volcado de datos para la tabla `tbl_usuarios`
+--
 
-ALTER TABLE tbl_reservas
-	ADD CONSTRAINT FK_usuarios_reservas FOREIGN KEY (id_usuarios)
-	REFERENCES tbl_usuarios(id_usuarios);
+INSERT INTO `tbl_usuarios` (`id_usuarios`, `nombre_usuarios`, `pwd_usuarios`, `estado_usuarios`, `admin_usuarios`, `informatico_usuarios`, `normal_usuarios`) VALUES
+(1, 'Gerard', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 1, 0, 0),
+(2, 'Sergio', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 1, 0, 0),
+(3, 'Victor', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 0, 1, 0),
+(4, 'Jesus', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 0, 1, 0),
+(5, 'Juanma', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 0, 0, 1),
+(6, 'Daniel', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 0, 0, 1);
 
-ALTER TABLE tbl_reservas
-	ADD CONSTRAINT FK_recursos_reservas FOREIGN KEY (id_recursos)
-	REFERENCES tbl_recursos(id_recursos);
+--
+-- Índices para tablas volcadas
+--
 
-ALTER TABLE tbl_recursos
-	ADD CONSTRAINT FK_tiporecursos_recursos FOREIGN KEY (id_tiporecursos)
-	REFERENCES tbl_tiporecursos(id_tiporecursos)
+--
+-- Indices de la tabla `tbl_indicencias`
+--
+ALTER TABLE `tbl_indicencias`
+  ADD PRIMARY KEY (`id_incidencias`),
+  ADD KEY `FK_usuarios_incidencias` (`id_usuarios`),
+  ADD KEY `FK_recursos_incidencias` (`id_recursos`);
 
+--
+-- Indices de la tabla `tbl_recursos`
+--
+ALTER TABLE `tbl_recursos`
+  ADD PRIMARY KEY (`id_recursos`),
+  ADD KEY `FK_tiporecursos_recursos` (`id_tiporecursos`);
 
+--
+-- Indices de la tabla `tbl_reservas`
+--
+ALTER TABLE `tbl_reservas`
+  ADD PRIMARY KEY (`id_reservas`),
+  ADD KEY `FK_usuarios_reservas` (`id_usuarios`),
+  ADD KEY `FK_recursos_reservas` (`id_recursos`);
 
+--
+-- Indices de la tabla `tbl_tiporecursos`
+--
+ALTER TABLE `tbl_tiporecursos`
+  ADD PRIMARY KEY (`id_tiporecursos`);
 
+--
+-- Indices de la tabla `tbl_usuarios`
+--
+ALTER TABLE `tbl_usuarios`
+  ADD PRIMARY KEY (`id_usuarios`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
+--
+-- AUTO_INCREMENT de la tabla `tbl_indicencias`
+--
+ALTER TABLE `tbl_indicencias`
+  MODIFY `id_incidencias` int(4) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `tbl_recursos`
+--
+ALTER TABLE `tbl_recursos`
+  MODIFY `id_recursos` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
+--
+-- AUTO_INCREMENT de la tabla `tbl_reservas`
+--
+ALTER TABLE `tbl_reservas`
+  MODIFY `id_reservas` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
+--
+-- AUTO_INCREMENT de la tabla `tbl_tiporecursos`
+--
+ALTER TABLE `tbl_tiporecursos`
+  MODIFY `id_tiporecursos` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
+--
+-- AUTO_INCREMENT de la tabla `tbl_usuarios`
+--
+ALTER TABLE `tbl_usuarios`
+  MODIFY `id_usuarios` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
+--
+-- Restricciones para tablas volcadas
+--
 
+--
+-- Filtros para la tabla `tbl_indicencias`
+--
+ALTER TABLE `tbl_indicencias`
+  ADD CONSTRAINT `FK_recursos_incidencias` FOREIGN KEY (`id_recursos`) REFERENCES `tbl_recursos` (`id_recursos`),
+  ADD CONSTRAINT `FK_usuarios_incidencias` FOREIGN KEY (`id_usuarios`) REFERENCES `tbl_usuarios` (`id_usuarios`);
 
+--
+-- Filtros para la tabla `tbl_recursos`
+--
+ALTER TABLE `tbl_recursos`
+  ADD CONSTRAINT `FK_tiporecursos_recursos` FOREIGN KEY (`id_tiporecursos`) REFERENCES `tbl_tiporecursos` (`id_tiporecursos`);
 
+--
+-- Filtros para la tabla `tbl_reservas`
+--
+ALTER TABLE `tbl_reservas`
+  ADD CONSTRAINT `FK_recursos_reservas` FOREIGN KEY (`id_recursos`) REFERENCES `tbl_recursos` (`id_recursos`),
+  ADD CONSTRAINT `FK_usuarios_reservas` FOREIGN KEY (`id_usuarios`) REFERENCES `tbl_usuarios` (`id_usuarios`);
+COMMIT;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
