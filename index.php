49a8af93886f3bffae407fed83541738ca7b8047
sergiorueda99 <55807705+sergiorueda99@users.noreply.github.com>
2019-11-07 16:58:55 +0100

    login mejorado

--- index.php
+++ index.php
@@ -11,17 +11,20 @@ and open the template in the editor.
     </head>
     <script type="text/javascript" src="js/codigo.js">
     </script>
-    <body>
-        <p id="alerta" class="alerta" style='text-align: center' ></p>
+    <body style="background-color: #FFDE90">
+        <center>
+        <br><br>
         <form id='login' action='login.proc.php' method='post' accept-charset='UTF-8' onsubmit = "return ValidacionLogin()">
-                <table>
+                <table style="border: solid 2px;border-radius: 12px; background-color: #FFD472; width: 500px;  ">
                     <tr>
-                        <th><legend>Login</legend></th>
+                        <th style="background-color: #FFC94D; border: 1px solid; border-radius: 12px;"><legend style="font-size: 40px;">Login</legend></th>
                         <th><input type='hidden' name='submitted' id='submitted' value='1'/></th>
                     </tr>
                     <tr>
-                        <th><label for='username' >Nombre de Usuario:</label></th>
-                        <th><input type='text' placeholder="Introduce el usuario" pattern="[a-z]{1,15}" name='username' id='username'  value="<?php 
+                        <th style="font-size: 30px;"><label for='username' >Nombre de Usuario</label></th>
+                        </tr>
+                        <tr>
+                        <th><input style="width: 290px; height: 30px; font-size: 25px;" type='text' placeholder="Introduce el usuario" pattern="[a-z]{1,15}" name='username' id='username'  value="<?php 
                                 if (isset($_GET['us'])) {
                                     $user=$_GET['us'];
                                     echo "$user";
@@ -29,13 +32,18 @@ and open the template in the editor.
                         ?>" maxlength="50"/></th>
                     </tr>
                     <tr>
-                        <th><label for='password' >Contraseña:</label></th>
-                        <th><input type='password' placeholder="Introduce contraseña" name='password' id='password' maxlength="50"/></th>
+                        <th style="font-size: 30px;"><label for='password' >Contraseña</label></th>
                     </tr>
                     <tr>
-                        <th><button class="button" type="submit">Entrar</button></th></th>
+                        <th><input style="width: 290px; height: 30px; font-size: 25px;" type='password' placeholder="Introduce contraseña" name='password' id='password' maxlength="50"/></th>
+                    </tr>
+                    
+                    <tr>
+                        <th><button style="height: 50px; font-size: 35px; text-decoration: none;width: 180px;padding: 5px;color: white;background-color: #FFC94D;border-radius: 6px;border: 1px solid black; margin-top: 190px; margin-bottom: 30px;" class="button" type="submit">Entrar</button></th></th>
                     </tr>
                 </table>
+                <p style="font-size: 25px;" id="alerta" class="alerta"></p>
         </form>
+        </center>
     </body>
 </html>
