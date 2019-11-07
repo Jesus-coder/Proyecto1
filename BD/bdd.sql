191e9009e19cb6a18a90ad22e62082810beb9527
Pazos99 <55807866+Pazos99@users.noreply.github.com>
2019-11-07 15:21:13 +0100

    Actulizacion de la base de datos

--- BD/bdd.sql
+++ BD/bdd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-11-2019 a las 16:33:41
--- Versión del servidor: 10.1.28-MariaDB
--- Versión de PHP: 5.6.32
+-- Tiempo de generación: 07-11-2019 a las 15:20:01
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -58,19 +58,19 @@ INSERT INTO `tbl_recursos` (`id_recursos`, `nombre_recursos`, `estado_recursos`,
 (1, 'Yoga', 'disponible', 1),
 (2, 'Baile', 'disponible', 1),
 (3, 'Presentaciones', 'disponible', 1),
-(4, 'Gimnasio', 'ocupado', 1),
-(5, 'Informatica_1', 'ocupado', 1),
+(4, 'Gimnasio', 'disponible', 1),
+(5, 'Informatica_1', 'disponible', 1),
 (6, 'Informatica_2', 'disponible', 1),
 (7, 'Cocina', 'disponible', 1),
-(8, 'Despacho_1', 'ocupado', 1),
-(9, 'Despacho_2', 'ocupado', 1),
+(8, 'Despacho_1', 'disponible', 1),
+(9, 'Despacho_2', 'disponible', 1),
 (10, 'Actos', 'disponible', 1),
 (11, 'Reuniones', 'disponible', 1),
-(12, 'Proyector_portatil_1', 'ocupado', 2),
-(13, 'Proyector_portatil_2', 'ocupado', 2),
+(12, 'Proyector_portatil_1', 'disponible', 2),
+(13, 'Proyector_portatil_2', 'disponible', 2),
 (14, 'Portatil_1', 'disponible', 2),
-(15, 'Portatil_2', 'ocupado', 2),
-(16, 'Portatil_3', 'ocupado', 2),
+(15, 'Portatil_2', 'disponible', 2),
+(16, 'Portatil_3', 'disponible', 2),
 (17, 'Mobil_1', 'disponible', 2),
 (18, 'Mobil_2', 'disponible', 2);
 
@@ -87,25 +87,6 @@ CREATE TABLE `tbl_reservas` (
   `id_usuarios` int(4) NOT NULL,
   `id_recursos` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `tbl_reservas`
---
-
-INSERT INTO `tbl_reservas` (`id_reservas`, `inicio_reservas`, `final_reservas`, `id_usuarios`, `id_recursos`) VALUES
-(1, '2019-10-31 00:00:00', '2019-10-31 00:00:00', 1, 4),
-(8, '2019-11-05 01:20:00', '2019-11-05 01:20:20', 1, 8),
-(9, '2019-11-05 01:55:50', '2019-11-05 15:52:00', 1, 1),
-(10, '2019-11-05 01:55:50', '2019-11-05 15:52:15', 1, 2),
-(11, '2019-11-05 02:00:55', '2019-11-05 15:52:28', 1, 3),
-(12, '2019-11-05 02:00:55', NULL, 1, 4),
-(13, '2019-11-05 13:10:14', NULL, 2, 12),
-(14, '2019-11-05 13:10:14', NULL, 2, 13),
-(15, '2019-11-05 13:28:00', NULL, 1, 9),
-(16, '2019-11-05 13:30:32', NULL, 1, 15),
-(17, '2019-11-05 13:30:32', NULL, 1, 16),
-(18, '2019-11-05 15:41:53', NULL, 1, 5),
-(19, '2019-11-05 15:42:23', NULL, 1, 8);
 
 -- --------------------------------------------------------
 
@@ -136,7 +117,7 @@ CREATE TABLE `tbl_usuarios` (
   `id_usuarios` int(4) NOT NULL,
   `nombre_usuarios` varchar(25) NOT NULL,
   `pwd_usuarios` varchar(50) NOT NULL,
-  `estado_usuarios` varchar(25) NOT NULL,
+  `estado_usuarios` int(1) NOT NULL,
   `admin_usuarios` int(1) NOT NULL,
   `informatico_usuarios` int(1) NOT NULL,
   `normal_usuarios` int(1) NOT NULL
@@ -147,12 +128,12 @@ CREATE TABLE `tbl_usuarios` (
 --
 
 INSERT INTO `tbl_usuarios` (`id_usuarios`, `nombre_usuarios`, `pwd_usuarios`, `estado_usuarios`, `admin_usuarios`, `informatico_usuarios`, `normal_usuarios`) VALUES
-(1, 'Gerard', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 1, 0, 0),
-(2, 'Sergio', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 1, 0, 0),
-(3, 'Victor', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 0, 1, 0),
-(4, 'Jesus', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 0, 1, 0),
-(5, 'Juanma', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 0, 0, 1),
-(6, 'Daniel', '81dc9bdb52d04dc20036dbd8313ed055', 'enabled', 0, 0, 1);
+(1, 'Gerard', '81dc9bdb52d04dc20036dbd8313ed055', 1, 1, 0, 0),
+(2, 'Sergio', '81dc9bdb52d04dc20036dbd8313ed055', 1, 1, 0, 0),
+(3, 'Victor', '81dc9bdb52d04dc20036dbd8313ed055', 1, 0, 1, 0),
+(4, 'Jesus', '81dc9bdb52d04dc20036dbd8313ed055', 1, 0, 1, 0),
+(5, 'Juanma', '81dc9bdb52d04dc20036dbd8313ed055', 1, 0, 0, 1),
+(6, 'Daniel', '81dc9bdb52d04dc20036dbd8313ed055', 1, 0, 0, 1);
 
 --
 -- Índices para tablas volcadas
@@ -213,7 +194,7 @@ ALTER TABLE `tbl_recursos`
 -- AUTO_INCREMENT de la tabla `tbl_reservas`
 --
 ALTER TABLE `tbl_reservas`
-  MODIFY `id_reservas` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_reservas` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_tiporecursos`
